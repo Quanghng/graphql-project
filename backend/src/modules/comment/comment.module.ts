d4654a34a81a366cd1dc4d901a0de90e73d786e6
Quanghng <90704201+Quanghng@users.noreply.github.com>
2025-04-09 12:47:12 +0200

    added queries/mutations

--- backend/src/modules/comment/comment.module.ts
+++ backend/src/modules/comment/comment.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { CommentService } from "./comment.service";
 import { CommentResolver } from "./comment.resolver";
 import { PrismaService } from "../prisma/prisma.service";
+import { CommentLoader } from "./loaders/comment.loader";
 
 
 @Module({
-  providers: [CommentService, CommentResolver, PrismaService]
+  providers: [CommentService, CommentResolver, CommentLoader, PrismaService]
 })
 export class CommentModule { }
--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserResolver } from "./user.resolver";
 import { UserService } from "./user.service";
 import { ThreadService } from "../thread/thread.service";
 import { PrismaService } from "../prisma/prisma.service";
+import { ThreadLoader } from "../thread/loaders/thread.loader";
 
 
 @Module({
-  providers: [UserService, UserResolver, ThreadService, PrismaService]
+  providers: [UserService, UserResolver, ThreadService, ThreadLoader, PrismaService]
 })
 export class UserModule { }
